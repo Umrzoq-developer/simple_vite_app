de86c02adfd16fffca15d089a5aa3d44e9042d4e
umrzoq_toshkentov <utoshkentov@gmail.com>
2022-03-26 22:08:21 +0500

    FEAT new application initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-black">
+      <h1 className="text-blue-900">HELLO, I'M A REACT DEVELOPER!</h1>
+      <p className="text-white">- Rohan Singh Rawat</p>
+    </div>
+  );
+}
+
+export default App;
