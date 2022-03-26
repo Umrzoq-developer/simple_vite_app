563e6001708458fba6dcee472f27b5af6f629933
umrzoq_toshkentov <utoshkentov@gmail.com>
2022-03-26 22:14:19 +0500

    FIX application

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black">
       <h1 className="text-blue-900">HELLO, I'M A REACT DEVELOPER!</h1>
-      <p className="text-white">- Rohan Singh Rawat</p>
+      <p className="text-white">- Toshkentov Umrzoq</p>
     </div>
   );
 }
